bdc8f6f2fe7f36666c86fa282b32e58d8827b55a
Vanqueen <quenumeliette@gmail.com>
2026-07-25 12:25:29 +0200

    mise à jour

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -11,6 +11,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const FROM = process.env.EMAIL_FROM ?? `HIGHFIVE Tournament <${process.env.SMTP_USER}>`;
+console.log(`Email sender: ${FROM}`);
 
 export const sendTempPassword = async ({ to, full_name, tournamentName, tempPassword }) => {
   await transporter.sendMail({
