8853149313a5e9872a2c2ccd0897ba696d9fd3ba
Vanqueen <quenumeliette@gmail.com>
2026-07-25 19:17:41 +0200

    mise à jour

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,8 +10,6 @@ const connectDB = async () => {
 
   try {
     const parsedUri = new URL(uri);
-    console.log(`📡 ..: ${parsedUri.protocol}//${parsedUri.host}${parsedUri.pathname}`);
-    console.log(`📡 MongoDB target: ${parsedUri.protocol}//${parsedUri.host}${parsedUri.pathname}`);
     mongoose.set('strictQuery', true);
     await mongoose.connect(uri, {
       serverApi: ServerApiVersion.v1,
